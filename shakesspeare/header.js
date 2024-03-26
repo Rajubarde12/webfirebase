2d5be30cc6328b2c49f90d0235b0e7b6de49d22b
Raju Barde <rajub.forbear@gmail.com>
2024-03-26 12:21:03 +0530

    some changes

--- shakesspeare/header.js
+++ shakesspeare/header.js
@@ -1,122 +1,112 @@
-[
-  {
-    "userData": "tested@gmail.com",
-    "bullingAddress": {
-      "address": "kodiyakhal,birul,bhikangaon ",
-      "lastName": "BARDE",
-      "commentas": "",
-      "phone": "+918461955042",
-      "pincode": "451331",
-      "city": "KHARGONE",
-      "state": "MP",
-      "firstName": "RAJU",
-      "email": "rajubarde54@gmail.com",
-      "country": "IN",
-      "company": "NA"
+{
+  "orderList": [
+    {
+      "userData": "tested@gmail.com",
+      "orderitem": [
+        {
+          "quantity": 1,
+          "id": "2003"
+        },
+        {
+          "quantity": 1,
+          "id": "2004"
+        },
+        {
+          "id": "2005",
+          "quantity": 1
+        }
+      ],
+      "bullingAddress": {
+        "company": "NA",
+        "lastName": "BARDE",
+        "email": "rajubarde54@gmail.com",
+        "country": "IN",
+        "city": "KHARGONE",
+        "state": "MP",
+        "commentas": "",
+        "pincode": "451331",
+        "firstName": "RAJU",
+        "phone": "+918461955042",
+        "address": "kodiyakhal,birul,bhikangaon "
+      }
     },
-    "orderitem": {
-      "id": "2003",
-      "quantity": 1
-    }
-  },
-  {
-    "userData": "tested@gmail.com",
-    "bullingAddress": {
-      "address": "kodiyakhal,birul,bhikangaon ",
-      "lastName": "BARDE",
-      "commentas": "",
-      "phone": "+918461955042",
-      "pincode": "451331",
-      "city": "KHARGONE",
-      "state": "MP",
-      "firstName": "RAJU",
-      "email": "rajubarde54@gmail.com",
-      "country": "IN",
-      "company": "NA"
+    {
+      "bullingAddress": {
+        "country": "IN",
+        "address": "kodiyakhal,birul,bhikangaon ",
+        "lastName": "BARDE",
+        "phone": "+918461955042",
+        "commentas": "",
+        "city": "KHARGONE",
+        "firstName": "RAJU",
+        "email": "rajubarde54@gmail.com",
+        "company": "NA",
+        "state": "MP",
+        "pincode": "451331"
+      },
+      "userData": "test12@gmail.com",
+      "orderitem": [
+        {
+          "id": "2005",
+          "quantity": 2
+        },
+        {
+          "id": "2004",
+          "quantity": 1
+        },
+        {
+          "quantity": 1,
+          "id": "2003"
+        }
+      ]
     },
-    "orderitem": {
-      "quantity": 1,
-      "id": "2004"
-    }
-  },
-  {
-    "userData": "tested@gmail.com",
-    "bullingAddress": {
-      "address": "kodiyakhal,birul,bhikangaon ",
-      "lastName": "BARDE",
-      "commentas": "",
-      "phone": "+918461955042",
-      "pincode": "451331",
-      "city": "KHARGONE",
-      "state": "MP",
-      "firstName": "RAJU",
-      "email": "rajubarde54@gmail.com",
-      "country": "IN",
-      "company": "NA"
+    {
+      "orderitem": [
+        {
+          "id": "2001",
+          "quantity": 1
+        }
+      ],
+      "userData": "tested@gmail.com",
+      "bullingAddress": {
+        "phone": "45444555",
+        "lastName": "test",
+        "state": "MP",
+        "pincode": "452001",
+        "company": "na",
+        "firstName": "raju",
+        "city": "indore",
+        "email": "tesr@gmail.com",
+        "address": "abcd xyz",
+        "country": "IN",
+        "commentas": "rktkttyl"
+      }
     },
-    "orderitem": {
-      "quantity": 1,
-      "id": "2005"
+    {
+      "userData": "tested@gmail.com",
+      "bullingAddress": {
+        "state": "MP",
+        "firstName": "testing",
+        "pincode": "55966969",
+        "phone": "+596969699",
+        "lastName": "testing",
+        "commentas": "",
+        "address": "testing,testing ",
+        "city": "KHARGONE",
+        "company": "NA",
+        "email": "main@gmail.com",
+        "country": "IN"
+      },
+      "orderitem": [
+        {
+          "quantity": 1,
+          "id": "2003"
+        },
+        {
+          "id": "2009",
+          "quantity": 1
+        }
+      ]
     }
-  },
-  {
-    "userData": "test12@gmail.com",
-    "bullingAddress": {
-      "lastName": "BARDE",
-      "pincode": "451331",
-      "state": "MP",
-      "country": "IN",
-      "address": "kodiyakhal,birul,bhikangaon ",
-      "firstName": "RAJU",
-      "city": "KHARGONE",
-      "commentas": "",
-      "company": "NA",
-      "phone": "+918461955042",
-      "email": "rajubarde54@gmail.com"
-    },
-    "orderitem": {
-      "id": "2005",
-      "quantity": 2
-    }
-  },
-  {
-    "userData": "test12@gmail.com",
-    "bullingAddress": {
-      "lastName": "BARDE",
-      "pincode": "451331",
-      "state": "MP",
-      "country": "IN",
-      "address": "kodiyakhal,birul,bhikangaon ",
-      "firstName": "RAJU",
-      "city": "KHARGONE",
-      "commentas": "",
-      "company": "NA",
-      "phone": "+918461955042",
-      "email": "rajubarde54@gmail.com"
-    },
-    "orderitem": {
-      "id": "2004",
-      "quantity": 1
-    }
-  },
-  {
-    "userData": "test12@gmail.com",
-    "bullingAddress": {
-      "lastName": "BARDE",
-      "pincode": "451331",
-      "state": "MP",
-      "country": "IN",
-      "address": "kodiyakhal,birul,bhikangaon ",
-      "firstName": "RAJU",
-      "city": "KHARGONE",
-      "commentas": "",
-      "company": "NA",
-      "phone": "+918461955042",
-      "email": "rajubarde54@gmail.com"
-    },
-    "orderitem": {
-      "quantity": 1,
-      "id": "2003"
-    }
-  }
-]
+  ]
+}
